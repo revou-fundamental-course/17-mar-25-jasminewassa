ae145ee79514399dea892ed61bedcd09ea4967cc
jasminews <jasmine.w.salsabila@gmail.com>
2025-03-25 01:53:34 +0800

    update js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function hitungLuas(){
-panjang = document.getElementById("txtPanjang").value;
-lebar = document.getElementById("txtLebar").value;
-Luas = panjang * lebar;
-document.getElementById("txtLuas").value =  Luas;
+function hitungPerkalian() {
+  let bilangan1 = parseFloat(document.getElementById("bilangan1").value);
+  let bilangan2 = parseFloat(document.getElementById("bilangan2").value);
+  let hasil = bilangan1 * bilangan2;
+  document.getElementById("hasil").textContent = hasil;
 }
