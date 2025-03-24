142f0b67b88ccbdde1cfd8beec79e232c5e8234c
jasminews <jasmine.w.salsabila@gmail.com>
2025-03-24 13:54:55 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function hitungLuas(){
+panjang = document.getElementById("txtPanjang").value;
+lebar = document.getElementById("txtLebar").value;
+Luas = panjang * lebar;
+document.getElementById("txtLuas").value =  Luas;
+}
